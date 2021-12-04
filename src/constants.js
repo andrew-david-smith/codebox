5effa9c3b1d361cd82cca3b45b5f20f5aad742a6
andrew-david-smith <thranriel@gmail.com>
2021-12-04 22:36:13 +0000

    Add crystal language

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
   DEFAULT_THEMES: ['light', 'dark'],
   COMMON_LANGUAGES: {
-    none: 'Auto-detect', apache: 'Apache', bash: 'Bash', cs: 'C#', cpp: 'C++', css: 'CSS', coffeescript: 'CoffeeScript', diff: 'Diff',
+    none: 'Auto-detect', apache: 'Apache', bash: 'Bash', cr: 'Crystal', cs: 'C#', cpp: 'C++', css: 'CSS', coffeescript: 'CoffeeScript', diff: 'Diff',
     go: 'Go', haskell: 'Haskell', html: 'HTML, XML', http: 'HTTP', json: 'JSON', java: 'Java', javascript: 'JavaScript', kotlin: 'Kotlin',
     less: 'Less', lua: 'Lua', makefile: 'Makefile', markdown: 'Markdown', nginx: 'Nginx', objectivec: 'Objective-C',
     php: 'PHP', perl: 'Perl', properties: 'Properties', python: 'Python', ruby: 'Ruby', rust: 'Rust', scss: 'SCSS',
